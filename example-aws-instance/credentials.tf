ae5c4dd45d6e643edfc13bc19725b9519d5c7427
KM SHIVANI CHAUDHARY <77600875+Shivani2013@users.noreply.github.com>
2022-08-12 20:50:09 +0530

    Update credentials.tf

--- example-aws-instance/credentials.tf
+++ example-aws-instance/credentials.tf
@@ -1,6 +1,6 @@
 provider "aws" {
 region = "${var.myregion}"
-access_key = "AKIA4M4YB5RL2N6PHGMT"
-secret_key = "4d6FMZIkoM5Gwpq7nYwbKSvhVoPk3aq0JX2HrUvT"
+access_key = "****************"
+secret_key =  "****************"
 }
 
